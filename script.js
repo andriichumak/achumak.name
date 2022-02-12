2228540a7478c1bc00bd146e6d94a99a0bb82dcc
Andy Chumak <andrii.chumak@gooddata.com>
2022-02-12 20:26:25 +0100

    Added latest job position

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 window.addEventListener('load', () => {
-	let topBtnTimeout = undefined;
-	const topBtn = document.querySelector('#top');
-	topBtn.addEventListener('click', e => {
-		e.preventDefault();
-		e.stopPropagation();
-		window.scroll({top: 0, behavior: 'smooth'});
-	});
-	document.addEventListener('scroll', e => {
-		clearTimeout(topBtnTimeout);
-		if (window.scrollY < window.innerHeight) {
-			hideTopButton();
-		} else {
-			if (!topBtnTimeout) {
-				topBtn.classList.add('top-in');
-			}
-			topBtnTimeout = setTimeout(hideTopButton, 3000);
-		}
-	});
-	function hideTopButton() {
-		topBtn.classList.remove('top-in');
-		topBtnTimeout = undefined;
-	}
-
-
 	const header = document.querySelector('header');
 	const menuBtn = header.querySelector('.menu-button');
 	const nav = header.querySelector('nav');
